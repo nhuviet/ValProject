4a7926f272889122da057ac38b4fec6d221c3aa6
Viet Nhu Tran <126634632+VietNhu@users.noreply.github.com>
2023-03-24 11:05:40 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+function CheckAuth(){
+    let user = getCookie("user");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } 
+    else {
+        window.location = "login.html"
+        if (user != "" && user != null) {
+          setCookie("username", user, 30);
+        }
+    }
+    
+}
+function setCookie(key,value,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = key + "=" + value + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(key) {
+    let name = key + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+  function login() {
+    let un = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+    const userLogin = {username: un, pass: pass};
+    const myJSON = JSON.stringify(userLogin);
+    setCookie("userLogin",myJSON,10);
+  }
+
+
+/*        if (user != "" && user != null) {
+            setCookie("username", user, 30);
+        }*/
